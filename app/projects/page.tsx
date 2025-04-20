f7cc6851235ffe2c7d7952a154b4437b91a4fe0e
Kevin <54258753+vCiKv@users.noreply.github.com>
2025-04-20 20:22:46 +0100

    update: project page design

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,15 +4,26 @@ import Image from "@/components/elements/image";
 import { SectionTitle } from "@/components/elements/title";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+
+function ProjectPill({ status }: { status: string | "ongoing" | "completed" }) {
+  const isCompleted = status === "completed"
+  const className = isCompleted ? "bg-green-400" : "bg-orange-300"
+  const icon = isCompleted ? "icon" : "icon"
+  return (
+    <span className={twMerge("px-2.5 py-0.5 text-sm font-medium text-center text-white rounded-full ", className)}>
+      {status} {icon}
+    </span>
+  );
+};
 const ourProjects = [
   {
     title: "Title-1",
     location: "Alakuko, Lagos.",
-    image: "/img/kvk.png",
+    image: ["/img/kvk.png",],
     paragraph:
       "Complete renovation of building and facilities upgrade of pipelines, tanks, power generator and electrical systems at Zone 4 service station,",
     tag: "knockdown and rebuild",
-    startData: 2012,
+    startDate: 2012,
     endDate: 2013,
     status: "ongoing",
     client: "client_name",
@@ -20,22 +31,22 @@ const ourProjects = [
   {
     title: "Title-2",
     location: "abuja",
-    image: "/img/kvk.png",
+    image: ["/img/kvk.png",],
     paragraph:
       "Construction of 15 units of fully detached houses at Nigerian union of teachers (NUT) Estate, Lugbe,  Abuja",
     tag: "civil construction",
-    startData: 2012,
+    startDate: 2012,
     endDate: 2013,
-    status: "ongoing",
+    status: "completed",
     client: "client_name",
   },
   {
     title: "Title-3",
     location: "lagos",
-    image: "/img/kvk.png",
+    image: ["/img/kvk.png",],
     paragraph:
       "Plumbing, water treatment and Renovation work at conoil expatriates quarters (block of 30 flats) at Victoria Island Lagos.",
-    startData: 2012,
+    startDate: 2012,
     endDate: 2013,
     status: "ongoing",
     client: "client_name",
@@ -50,10 +61,10 @@ const stats = [
 const Projects = (props: {
   title: string;
   location: string;
-  image: string;
+  image: string[];
   paragraph: string;
   tag?: string;
-  startData: number;
+  startDate: number;
   endDate: number;
   status: string;
   client: string;
@@ -61,14 +72,21 @@ const Projects = (props: {
   return (
     <div className="h-full px-5 group/image md:px-0">
       <Box className="flex flex-col h-full">
-        <div className="relative block object-contain w-full duration-200 ease-in scale-95 rounded-md h-28 group-hover/image:scale-100">
-          <Image src={props.image} className="object-cover w-full h-full" />
+        <div className="relative block object-contain w-full duration-200 ease-in scale-95 h-56 group-hover/image:scale-100">
+          <Image src={props.image[0]} className="object-cover w-full h-full" />
         </div>
-        <div className="my-4 tracking-tight">
-          <p className="-my-1 text-sm font-light capitalize">
+        <div className="py-4 tracking-tight">
+          <p className="py-1 text-sm font-light capitalize">
             {props.location}
           </p>
-          <h3 className="mb-2 text-2xl font-medium">{props.title}</h3>
+          <h3 className="pb-2 text-2xl font-medium">{props.title}</h3>
+          <h6><b className="font-semibold">Client: </b> {props.client}</h6>
+          <div className="flex flex-col items-end w-full justify-center text-sm gap-1">
+            <span><b className="font-semibold">Start Date: </b>{props.startDate}</span>
+            <span><b className="font-semibold">End Date: </b>{props.endDate}</span>
+            <ProjectPill status={props.status} />
+          </div>
+
         </div>
         <p className="pb-10 leading-relaxed">{props.paragraph}</p>
         {/* {props.tag&&<div className="px-3 py-1 text-xs font-medium text-center text-white rounded-full w-28 bg-primary">
