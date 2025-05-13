aa5aa016317430d2f7de218961d85fc526db769f
Kevin <54258753+vCiKv@users.noreply.github.com>
2025-05-13 04:30:12 +0100

    add: new icons

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,15 +2,16 @@
 import Box from "@/components/elements/box";
 import Image from "@/components/elements/image";
 import { SectionTitle } from "@/components/elements/title";
+import { Avatar, CheckOne, EveryUser, Notepad, Timer } from "@icon-park/react";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 function ProjectPill({ status }: { status: string | "ongoing" | "completed" }) {
   const isCompleted = status === "completed"
   const className = isCompleted ? "bg-green-400" : "bg-orange-300"
-  const icon = isCompleted ? "icon" : "icon"
+  const icon = isCompleted ? <CheckOne /> : <Timer />
   return (
-    <span className={twMerge("px-2.5 py-0.5 text-sm font-medium text-center text-white rounded-full ", className)}>
+    <span className={twMerge("inline-flex justify-center items-center gap-1 px-2.5 py-0.5 text-sm font-medium text-center text-white rounded-full ", className)}>
       {status} {icon}
     </span>
   );
@@ -18,40 +19,83 @@ function ProjectPill({ status }: { status: string | "ongoing" | "completed" }) {
 function ProjectConstruction(props: { isHidden?: boolean }) {
   const ourProjectsConstruction = [
     {
-      title: "Title-1",
+      title: "Zone 4 service station",
       location: "Alakuko, Lagos.",
-      image: ["/img/kvk.png",],
+      image: ["/img/kvk.png"],
       paragraph:
         "Complete renovation of building and facilities upgrade of pipelines, tanks, power generator and electrical systems at Zone 4 service station,",
       tag: ["knockdown and rebuild"],
-      startDate: 2012,
-      endDate: 2013,
-      status: "ongoing",
-      client: "client_name",
+      startDate: 2015,
+      endDate: 2015,
+      status: "completed",
+      client: "Zone 4",
     },
     {
       title: "Title-2",
-      location: "abuja",
+      location: "Lugbe, Abuja",
       image: ["/img/kvk.png",],
       paragraph:
         "Construction of 15 units of fully detached houses at Nigerian union of teachers (NUT) Estate, Lugbe,  Abuja",
-      tag: "civil construction",
+      tag: ["civil construction"],
       startDate: 2012,
       endDate: 2013,
       status: "completed",
       client: "client_name",
     },
     {
-      title: "Title-3",
-      location: "lagos",
+      title: "Renovation work at CONOIL",
+      location: "Victoria Island, Lagos.",
       image: ["/img/kvk.png",],
       paragraph:
         "Plumbing, water treatment and Renovation work at conoil expatriates quarters (block of 30 flats) at Victoria Island Lagos.",
+      startDate: 2013,
+      endDate: 2013,
+      status: "completed",
+      client: "CONOIL PLC",
+    },
+    {
+      title: "Mechanical and electrical consultancy services for AA Rano",
+      location: "Satellite town, Lagos.",
+      image: ["/img/kvk.png",],
+      paragraph:
+        "Mechanical and electrical consultancy services for AA Rano  70 million litres capacity tank farm, Ijegun, satellite town, Lagos.",
       startDate: 2012,
       endDate: 2013,
-      status: "ongoing",
-      client: "client_name",
-
+      status: "completed",
+      client: "A.A RANO NIG LTD",
+    },
+    {
+      title: "Rebuild filling station for AA Rano Ltd",
+      location: "Iyana Ipaja, Lagos.",
+      image: ["/img/kvk.png",],
+      paragraph:
+        "Complete Knock down and rebuild filling station project former Amo oil to a mega station for AA Rano Ltd,  Iyana Ipaja.",
+      startDate: 2014,
+      endDate: 2014,
+      status: "completed",
+      client: "A.A RANO NIG LTD",
+    },
+    {
+      title: "Wall fencing and foundation works for service station",
+      location: "Isheri, Lagos.",
+      image: ["/img/kvk.png",],
+      paragraph:
+        "Retaining wall fencing and foundation works for service station at  Lasu- Iba express road, Isheri, Lagos.",
+      startDate: 2015,
+      endDate: 2015,
+      status: "completed",
+      client: "A.A RANO NIG LTD",
+    },
+    {
+      title: "5,000 Cubic meters LPG tank farm",
+      location: "Satellite Town, Lagos.",
+      image: ["/img/kvk.png",],
+      paragraph:
+        "Project supervision works at AA RANO 5,000 Cubic meters LPG tank farm at Ijegun, satellite town ",
+      startDate: 2017,
+      endDate: 2019,
+      status: "completed",
+      client: "A.A RANO NIG LTD",
     },
   ];
   if (props.isHidden) {
@@ -68,14 +112,15 @@ function ProjectConstruction(props: { isHidden?: boolean }) {
               <p className="py-1 text-sm font-light capitalize">
                 {project.location}
               </p>
-              <h3 className="pb-2 text-2xl font-medium">{project.title}</h3>
-              <h6><b className="font-semibold">Client: </b> {project.client}</h6>
-              <div className="flex flex-col items-end w-full justify-center text-sm gap-1">
-                <span><b className="font-semibold">Start Date: </b>{project.startDate}</span>
-                <span><b className="font-semibold">End Date: </b>{project.endDate}</span>
-                <ProjectPill status={project.status} />
+              <h3 className="pb-2 text-2xl font-medium md:h-32">{project.title}</h3>
+              <div className="flex flex-col md:flex-row">
+                <h6><b className="font-semibold">Client: </b> {project.client}</h6>
+                <div className="flex flex-col items-end w-full justify-center text-sm gap-1">
+                  <span><b className="font-semibold">Start Date: </b>{project.startDate}</span>
+                  <span><b className="font-semibold">End Date: </b>{project.endDate}</span>
+                  <ProjectPill status={project.status} />
+                </div>
               </div>
-
             </div>
             <p className="pb-10 leading-relaxed">{project.paragraph}</p>
             {/* {props.tag&&<div className="px-3 py-1 text-xs font-medium text-center text-white rounded-full w-28 bg-primary">
@@ -158,7 +203,7 @@ function ProjectFacilities(props: { isHidden?: boolean }) {
               <p className="py-1 text-sm font-light capitalize">
                 {project.location}
               </p>
-              <h6 className="font-medium text-lg">{project.client}</h6>
+              <h6 className="font-medium text-lg pb-4">{project.client}</h6>
               <div className="flex flex-col items-end w-full justify-center text-sm gap-1">
                 <span>{project.startDate} - {project.endDate}</span>
                 <ProjectPill status={project.status} />
@@ -175,9 +220,9 @@ function ProjectFacilities(props: { isHidden?: boolean }) {
   }
 }
 const stats = [
-  { text: "projects", value: "100+", icon: "icon" },
-  { text: "clients", value: "60+", icon: "icon" },
-  { text: "employees", value: "20+", icon: "icon" },
+  { text: "projects", value: "100+", icon: <Notepad size={28} className="text-accent" /> },
+  { text: "clients", value: "60+", icon: <Avatar size={28} className="text-accent" /> },
+  { text: "employees", value: "20+", icon: <EveryUser size={28} className="text-accent" /> },
 ];
 
 export default function Page() {
@@ -195,7 +240,7 @@ export default function Page() {
         <div>
           <div className="grid items-stretch justify-start grid-cols-3 gap-1 my-10 md:grid-cols-3 flex-nowrap">
             {stats.map((stat) => (
-              <div key={stat.text} className="flex flex-col gap-2 text-center">
+              <div key={stat.text} className="flex flex-col gap-2 text-center justify-center items-center">
                 <span>{stat.icon}</span>
                 <span className="text-5xl font-black text-accent ">
                   {stat.value}
